0b6e7293c401da3564698888b739339482897881
LeoDiNino97 <dinino.1919479@studenti.uniroma1.it>
2024-05-20 11:56:24 +0200

    Update

--- Social Networks/Homework/P6/train_utils.py
+++ Social Networks/Homework/P6/train_utils.py
@@ -28,7 +28,7 @@ def train(model, optimizer, data):
 
 # Define the testing function
 @torch.no_grad()  # Disable gradient computation for testing
-def test(model, data):
+def node_classification_test(model, data):
 
     # Set the model to evaluation mode
     model.eval() 
@@ -41,12 +41,12 @@ def test(model, data):
     # Calculate accuracy for training, validation, and test sets
     for mask in [data.train_mask, data.val_mask, data.test_mask]:
         accs.append(int((pred[mask] == data.y[mask]).sum()) / int(mask.sum()))
+
     return accs
 
 def node_classification_train_loop(model, optimizer, data, epochs, patience = False):
     # Initialize variables to keep track of the best validation accuracy and test accuracy
     best_val_acc = 0
-    test_acc = 0
     train_loss = []
     val_accs = []
 
@@ -55,15 +55,15 @@ def node_classification_train_loop(model, optimizer, data, epochs, patience = Fa
         loss = train(model, optimizer, data)  # Train the model
         train_loss.append(loss)
 
-        _, val_acc, tmp_test_acc = test(model, data)  # Test the model
+        _, val_acc, _ = node_classification_test(model, data)  # Test the model
         val_accs.append(val_acc)
 
+        if epoch % 5 == 0:
+            print(f'Epoch: {epoch:<5} | Training Loss: {loss:.4f} | Validation accuracy: {val_acc:.4f}')
         # Update the best validation accuracy and corresponding test accuracy
         if patience:
             if val_acc > best_val_acc:
                 best_val_acc = val_acc
-                test_acc = tmp_test_acc
-                best_epoch = epoch
 
                 # Reset early stopping counter
                 early_stop_counter = 0  
@@ -78,12 +78,10 @@ def node_classification_train_loop(model, optimizer, data, epochs, patience = Fa
             # Check for early stopping
             if early_stop_counter >= patience:
                 print(f'Early stopping at epoch {epoch} with best validation accuracy: {best_val_acc:.4f}')
-                print(f'Test accuracy at epoch {best_epoch}: {test_acc:.4f}')
-                break
-        else:
-            test_acc = tmp_test_acc 
 
-    return train_loss, val_accs, test_acc
+                break
+
+    return train_loss, val_accs
 
 # ______________________________________________LINK PREDICTION TASK________________________________________
 
